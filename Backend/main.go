06581db6e584a5a7753c635d7c5fe5a764dd4b76
Lerdwut <guidehi1256@gmail.com>
2025-04-11 16:46:27 +0700

    connect Supabase DB

--- Backend/main.go
+++ Backend/main.go
@@ -1,11 +1,28 @@
 package main
 
 import (
+    "context"
+    "fmt"
+    "github.com/jackc/pgx/v5/pgxpool"
+    "log"
+    "os"
     "github.com/gofiber/fiber/v2"
     "Backend/routes"
 )
 
+var DB *pgxpool.Pool
+
 func main() {
+    dbURL := os.Getenv("SUPABASE_DB_URL")
+
+    var err error
+    DB, err = pgxpool.New(context.Background(), dbURL)
+    if err != nil {
+        log.Fatal("Can't Connect tp Supabase DB:", err)
+    }
+
+    fmt.Println("Connect to Supabase Success")
+
     app := fiber.New()
     routes.SetupRoutes(app)
     app.Listen(":3000")
